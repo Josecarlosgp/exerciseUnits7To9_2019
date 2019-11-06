d2721c23ee4e54cfa205af5388428cafde9849f1
Josecarlosgp <jotace300@gmail.com>
2019-11-06 13:28:17 +0100

    1º push FinalMark

---
+++ CheckFinalMark.cpp
@@ -0,0 +1,37 @@
+// Jose Carlos GIner Poveda
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int MAX=10;
+    int numbers[MAX];
+    int increments=0;
+    int decreases=0;
+    
+    cout<<"Enter 10 numbers to know the increments and dreceases in pairs."<<endl;
+    
+    for(int n=0; n<MAX;n++)
+    {
+        cout << "Enter the number"<<" "<<n+1<<": ";
+        cin >> numbers[n];
+        if(numbers[n]<0)
+        {
+            cout <<"Only possitive numbers please"<<endl;
+            n--;
+        }
+    }
+    for(int j=0;j<MAX-1;j++)
+    {
+        if(numbers[j] < numbers [j+1])
+        {
+            increments++;
+        }
+        else if(numbers[j] > numbers [j+1])
+        {
+            decreases++;
+        }
+    }
+    cout <<"Increments: " << increments<<endl;
+    cout <<"decreases: " << decreases<<endl;
+}
+            
